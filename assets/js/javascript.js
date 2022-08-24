9faf59e8ed64e3d72968201289584af557dc132f
Daniela Lopez <dlope0831@gmail.com>
2022-08-23 19:03:17 -0500

    attempting to add the uv

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -4,9 +4,9 @@ var cityInputEl = document.querySelector("#city");
 var cityContainerEl = document.querySelector("#city-container");
 var citySearchTerm = document.querySelector("#city-search-term");
 
-var cityHistory = function (){
-  JSON.parse(localStorage.getItem(city));
-};
+// var cityHistory = function (){
+//   JSON.parse(localStorage.getItem(city));
+// };
 
 var formSubmitHandler = function(event) {
     // prevent page from refreshing
@@ -23,7 +23,7 @@ var formSubmitHandler = function(event) {
       cityInputEl.value = "";
     } else {
       alert("Please enter a city");
-      localStorage.setItem(city, JSON.stringify(city))
+      //localStorage.setItem(city, JSON.stringify(city))
       cityContainerEl.innerHTML = `<h3> ${city} </h3>`;
     }
 
@@ -36,10 +36,11 @@ var formSubmitHandler = function(event) {
     getWeatherData(event.target.value)
 
   }
-  document.querySelectorAll("#city-buttons .btn").forEach(function(btn) {
-    btn.addEventListener("click", cityButtonHandler)
-    // console.log(btn.value)
-  });
+  
+  // document.querySelectorAll("#city-buttons .btn").forEach(function(btn) {
+  //   btn.addEventListener("click", cityButtonHandler)
+  //   // console.log(btn.value)
+  // });
 
   var getWeatherData = function (searchQuery) {
     var currentApiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + searchQuery + "&units=imperial&APPID=791d4e5cacae8c82a3c4abaa9b7629bf";
@@ -48,6 +49,7 @@ var formSubmitHandler = function(event) {
         response.json().then(function(currentData) {
           console.log(currentData);
           displayWeather(currentData);
+          displayUv(currentData);
         })
       }
     })
@@ -56,12 +58,23 @@ var formSubmitHandler = function(event) {
         if (response.ok) {
          response.json().then(function(data){
             console.log(data);
-            var oneApiUrl ="https://api.openweathermap.org/data/2.5/forecast?lat=" + data[0].lat + "&lon=" + data[0].lon +"&exclude=hourly,daily&appid=791d4e5cacae8c82a3c4abaa9b7629bf"
+            var oneApiUrl ="https://api.openweathermap.org/data/2.5/uvi?lat=" + data[0].lat + "&lon=" + data[0].lon +"&exclude=hourly,daily&appid=791d4e5cacae8c82a3c4abaa9b7629bf"
             fetch(oneApiUrl).then(function(response) {
               if (response.ok) {
                 response.json().then(function(weatherData){
                   console.log(weatherData);
-                  
+                  // console.log(weatherData.value)
+                  // cityContainerEl.innerHTML = `<h3> UV: ${weatherData.value} </h3>`
+
+                  // var UVQueryURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + data[0].lat + "&lon=" + data[0].lon +"&exclude=hourly,daily&appid=791d4e5cacae8c82a3c4abaa9b7629bf";
+                  // fetch(UVQueryURL).then(function(response) {
+                  //   if (response.ok) {
+                  //     response.json().then(function(uvIndex) {
+                  //       console.log(uvIndex);
+                  //     }
+                  // )}
+                  // })
+                
                 })
               }
             })
@@ -69,21 +82,89 @@ var formSubmitHandler = function(event) {
         }
     })
 }
-// So you would basically just store an array of city names in local storage and then create html elements for the things in that array.
+  
 
     var displayWeather = function(data) {
-      cityContainerEl.innerHTML = `<h3> ${data.name} </h3>
-      <h5> Temp: ${data.main.temp} </h5>
-      <h4> Humidity: ${data.main.humidity} </h4>
-      <h7> ${data.weather[0].main} </h7> `
-  
+      cityContainerEl.innerHTML = `<h3> ${data.name} </h3> 
+      <li> Temp: ${data.main.temp} F</li>
+      <li> Humidity: ${data.main.humidity}%</li>
+      <li> Wind: ${data.wind.speed} mph</li>
+      <li> Sky: ${data.weather[0].main} </li>`
+
+      var displayUv = function(weatherData) {
+        displayUv = [""];
+        cityContainerEl.innerHTML = `<h3> ${weatherData.value} </h3>`
+
+        // <li> Temp: ${data.main.temp} F</li>
+        // <li> Humidity: ${data.main.humidity}%</li>
+        // <li> Wind: ${data.wind.speed} mph</li>
+        // <li> Sky: ${data.weather[0].main} </li>`
+      
+
+      // var displayUv = function(weatherData) {
+      //     displayUv = "";
+        
+       
+      // }
+
+
+      // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      // We check to see if there's anything in the local storage for cities
+      var citiesArray = JSON.parse(localStorage.getItem("cities")) || [];
+      // First, we want to make sure the city hasn't already been added to the list
+      if (!citiesArray.includes(data.name.toLowerCase())){
+        // If this city is not in the list of cities, we add it
+        citiesArray.push(data.name.toLowerCase());
+        // Now we store the array in localStorage
+        localStorage.setItem("cities", JSON.stringify(citiesArray));
+        // Now we need to re-add the buttons
+        addSearchHistory();  
+      }
+      
+      // If the city IS already in the list, we don't add it again      
+    }}
+
+    var capitalizeThis = function(word) {
+      // This takes a word like "new york" and turns it
+      // into ["new", "york"]
+      // This way, we can capitalize each individual word
+      let individualWords = word.split(" ");
+
+      // This loops through the cities and creates a new
+      // array where the first letter of the word is capitalized
+      // ["new", "york"] becomes ["New", "York"]
+      let capitalizedWords =  individualWords.map(function (w) {
+        return w[0].toUpperCase() + w.slice(1);
+      })
+
+      // We use join to turn ["New", "York"] as "New York"
+      return capitalizedWords.join(" ");
     }
 
+    var addSearchHistory = function(){
+      cityButtonsEl.innerHTML = "";
+
+      // If there are cities stored in local storage, we
+      // set citiesArray to it
+      // Otherwise, we use the example cities you have.
+      var citiesArray = JSON.parse(localStorage.getItem("cities")) || ["austin", "chicago", "new york", "orlando", "san francisco", "seattle"];
+
+      citiesArray.forEach(function(city){
+        // Create a new button
+        let newButton = document.createElement("button");
+        // Make the button's text be the city name
+        newButton.textContent = capitalizeThis(city);
+        // Set the button's value to the city name
+        newButton.value = city.toLowerCase();
+        newButton.addEventListener("click", cityButtonHandler);
+        // Now we add the button to the page
+        cityButtonsEl.appendChild(newButton);
+      })
+
+      // Setting this in case there was nothing stored in localStorage
+      localStorage.setItem("cities", JSON.stringify(citiesArray))
+    }
+
+  // When the page first loads, we add search history
+  addSearchHistory();
   userFormEl.addEventListener("submit", formSubmitHandler);
-
-
-
-
-
-
-
